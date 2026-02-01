d8eb71ebae6745582230fcacb01a0cfbd89d9d42
Taylor Fausak <taylor@fausak.me>
2026-01-31 22:27:06 -0600

    Add a devcontainer

--- nixpkgs.nix
+++
@@ -1,4 +0,0 @@
-import (fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/871b9fd269ff6246794583ce4ee1031e1da71895.tar.gz";
-  sha256 = "sha256-M101xMtWdF1eSD0xhiR8nG8CXRlHmv6V+VoY65Smwf4=";
-}) { }
--- shell.nix
+++ shell.nix
@@ -1,6 +1,10 @@
 # https://nix.dev/tutorials/first-steps/declarative-shell.html
 let
-  pkgs = import ./nixpkgs.nix;
+  nixpkgs = builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/fa83fd837f3098e3e678e6cf017b2b36102c7211.tar.gz";
+    sha256 = "sha256-e7VO/kGLgRMbWtpBqdWl0uFg8Y2XWFMdz0uUJvlML8o=";
+  };
+  pkgs = import nixpkgs {};
 in
 pkgs.mkShell {
   nativeBuildInputs = [

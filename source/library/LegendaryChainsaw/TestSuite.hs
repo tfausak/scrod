1b78dc06fe685820a712f4ad8a55c6f8d233065a
Taylor Fausak <taylor@fausak.me>
2026-02-03 15:47:02 +0000

    Add a test suite

--- source/library/LegendaryChainsaw/TestSuite.hs
+++ source/library/LegendaryChainsaw/TestSuite.hs
@@ -1,4 +1,8 @@
 module LegendaryChainsaw.TestSuite where
 
-testSuite :: IO ()
-testSuite = pure ()
+import qualified LegendaryChainsaw.Spec as Spec
+import qualified LegendaryChainsaw.Version
+
+testSuite :: (Applicative m, Monad n) => Spec.Spec m n -> n ()
+testSuite s = do
+  LegendaryChainsaw.Version.spec s
--- source/test-suite/Main.hs
+++ source/test-suite/Main.hs
@@ -1,4 +1,18 @@
+import qualified Control.Monad.Trans.Writer as Writer
 import qualified LegendaryChainsaw
+import qualified LegendaryChainsaw.Spec as Spec
+import qualified Test.Tasty as Tasty
+import qualified Test.Tasty.HUnit as Unit
 
 main :: IO ()
-main = LegendaryChainsaw.testSuite
+main = Tasty.defaultMain testTree
+
+testTree :: Tasty.TestTree
+testTree = Tasty.testGroup "legendary-chainsaw" . Writer.execWriter $ LegendaryChainsaw.testSuite tasty
+
+tasty :: Spec.Spec IO (Writer.Writer [Tasty.TestTree])
+tasty = Spec.MkSpec
+  { Spec.assertFailure = Unit.assertFailure
+  , Spec.describe = \ s -> Writer.tell . pure . Tasty.testGroup s . Writer.execWriter
+  , Spec.it = \ s -> Writer.tell . pure . Unit.testCase s
+  }

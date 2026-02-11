dd23976d6c4e155ad60c2c09606d6c4a77b17377
Taylor Fausak <taylor@fausak.me>
2026-02-11 01:51:17 +0000

    Use absolute path

--- source/library/Scrod/Bootstrap.hs
+++ source/library/Scrod/Bootstrap.hs
@@ -5,13 +5,15 @@ module Scrod.Bootstrap where
 import qualified Data.Text as Text
 import qualified Language.Haskell.TH as TH
 import qualified Language.Haskell.TH.Syntax as TH
+import qualified System.Directory as Directory
 
 css :: Text.Text
 css =
   Text.pack
     $( do
          let filePath = "extra/bootstrap/bootstrap.min.css"
-         TH.addDependentFile filePath
+         absolutePath <- TH.runIO $ Directory.makeAbsolute filePath
+         TH.addDependentFile absolutePath
          content <- TH.runIO $ readFile filePath
          TH.lift content
      )

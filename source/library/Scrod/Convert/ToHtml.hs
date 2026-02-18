c93303bc2276e68ea001bb74db30251bf069c518
Taylor Fausak <tfausak@mercury.com>
2026-02-18 14:49:31 +0000

    Fix formatting

--- source/library/Scrod/Convert/ToHtml.hs
+++ source/library/Scrod/Convert/ToHtml.hs
@@ -153,12 +153,12 @@ bodyElement x =
             <> foldMap (List.singleton . sinceContent) (Module.since x)
             <> docContents (Module.documentation x)
             <> ( if Maybe.isNothing (Module.language x) && Map.null (Module.extensions x)
-                  then []
-                  else [element "section" [("class", "my-3")] . extensionsContents (Module.language x) $ Module.extensions x]
+                   then []
+                   else [element "section" [("class", "my-3")] . extensionsContents (Module.language x) $ Module.extensions x]
                )
             <> ( if null (Module.imports x)
-                  then []
-                  else [element "section" [("class", "my-3")] . importsContents $ Module.imports x]
+                   then []
+                   else [element "section" [("class", "my-3")] . importsContents $ Module.imports x]
                )
             <> [element "section" [("class", "my-3")] $ declarationsContents (Module.exports x) (Module.items x)]
             <> [footerContent x]

1fd0510dee5f584410749ed698cba552d179f77e
Taylor Fausak <taylor@fausak.me>
2026-02-23 14:51:04 +0000

    Remove HLint exception

--- source/library/Scrod/Extra/Builder.hs
+++ source/library/Scrod/Extra/Builder.hs
@@ -1,5 +1,3 @@
-{- hlint ignore "Avoid restricted extensions" -}
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskellQuotes #-}
 
 module Scrod.Extra.Builder where
@@ -23,49 +21,57 @@ toString = Text.unpack . toText
 spec :: (Applicative m, Monad n) => Spec.Spec m n -> n ()
 spec s = do
   Spec.named s 'toByteString $ do
+    let f = toByteString . Builder.stringUtf8
+    let b = ByteString.pack
+
     Spec.it s "works with an empty builder" $ do
-      Spec.assertEq s (toByteString "") ""
+      Spec.assertEq s (f "") $ b []
 
     Spec.it s "works with one byte" $ do
-      Spec.assertEq s (toByteString " \x0 ") " \x0 "
+      Spec.assertEq s (f " \x0 ") $ b [0x20, 0x00, 0x20]
 
     Spec.it s "works with two bytes" $ do
-      Spec.assertEq s (toByteString " \x80 ") " \xc2\x80 "
+      Spec.assertEq s (f " \x80 ") $ b [0x20, 0xc2, 0x80, 0x20]
 
     Spec.it s "works with three bytes" $ do
-      Spec.assertEq s (toByteString " \x800 ") " \xe0\xa0\x80 "
+      Spec.assertEq s (f " \x800 ") $ b [0x20, 0xe0, 0xa0, 0x80, 0x20]
 
     Spec.it s "works with four bytes" $ do
-      Spec.assertEq s (toByteString " \x10000 ") " \xf0\x90\x80\x80 "
+      Spec.assertEq s (f " \x10000 ") $ b [0x20, 0xf0, 0x90, 0x80, 0x80, 0x20]
 
   Spec.named s 'toText $ do
+    let f = toText . Builder.stringUtf8
+    let t = Text.pack
+
     Spec.it s "works with an empty builder" $ do
-      Spec.assertEq s (toText "") ""
+      Spec.assertEq s (f "") $ t ""
 
     Spec.it s "works with one byte" $ do
-      Spec.assertEq s (toText " \x0 ") " \x0 "
+      Spec.assertEq s (f " \x0 ") $ t " \x0 "
 
     Spec.it s "works with two bytes" $ do
-      Spec.assertEq s (toText " \x80 ") " \x80 "
+      Spec.assertEq s (f " \x80 ") $ t " \x80 "
 
     Spec.it s "works with three bytes" $ do
-      Spec.assertEq s (toText " \x800 ") " \x800 "
+      Spec.assertEq s (f " \x800 ") $ t " \x800 "
 
     Spec.it s "works with four bytes" $ do
-      Spec.assertEq s (toText " \x10000 ") " \x10000 "
+      Spec.assertEq s (f " \x10000 ") $ t " \x10000 "
 
   Spec.named s 'toString $ do
+    let f = toString . Builder.stringUtf8
+
     Spec.it s "works with an empty builder" $ do
-      Spec.assertEq s (toString "") ""
+      Spec.assertEq s (f "") ""
 
     Spec.it s "works with one byte" $ do
-      Spec.assertEq s (toString " \x0 ") " \x0 "
+      Spec.assertEq s (f " \x0 ") " \x0 "
 
     Spec.it s "works with two bytes" $ do
-      Spec.assertEq s (toString " \x80 ") " \x80 "
+      Spec.assertEq s (f " \x80 ") " \x80 "
 
     Spec.it s "works with three bytes" $ do
-      Spec.assertEq s (toString " \x800 ") " \x800 "
+      Spec.assertEq s (f " \x800 ") " \x800 "
 
     Spec.it s "works with four bytes" $ do
-      Spec.assertEq s (toString " \x10000 ") " \x10000 "
+      Spec.assertEq s (f " \x10000 ") " \x10000 "
